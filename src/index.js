bca04caa7be9a26b20b693444915b2037b7f0c4d
Thomas Merlini <thomas.merlini@studenti.isissgobetti.it>
2023-04-19 09:11:49 +0200

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -34,21 +34,3 @@ class GestioneFileSynk {
   };
 }
 
-let gFs = new GestioneFileSynk("./src/prova.txt");
-// gFs.WriteFile("Ciao");
-// gFs.WriteFile("Come stai?");
-// gFs.WriteFile("Bene?");
-let data = gFs.ReadFile();
-console.log("File prova.txt:1", data)
-// let lines = data.split(/\r?\n/);
-let lines = data.split(os.EOL);
-console.log("Righe:1", lines);
-
-let gFs1 = new GestioneFileSynk("./src/prova1.txt");
-gFs1.WriteFile("")
-data = gFs1.ReadFile();
-console.log("prova1.txt:1", data);
-// gFs1.WriteFile("prova Ciao");
-// gFs1.AppendFile("Get Up!!")
-// data = gFs1.ReadFile();
-// console.log("File prova1.txt:2", data);
